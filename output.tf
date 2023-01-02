a08db10d934545cd9fe8e12eac34d698ba62606e
Rafael.Goncalves <ragsilva.ciet@rd.com.br>
2023-01-02 10:01:41 -0300

    export ID

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "bucket_arn"{
     value = aws_s3_bucket.rafaelsilva4_module.arn
 }
+
+output "s3_bucket_id" {
+  description = "The name of the bucket."
+  value       = aws_s3_bucket.rafaelsilva4_module.id
+}
