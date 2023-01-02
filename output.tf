61a831421c0834ccf54d26fa45ed45b8decfa1e9
Rafael.Goncalves <ragsilva.ciet@rd.com.br>
2023-01-02 10:33:05 -0300

    change name

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "bucket_arn"{
-    value = aws_s3_bucket.rafaelsilva4module.arn
+    value = aws_s3_bucket.rafaelsilva4-module.arn
 }
 
 output "s3_bucket_id" {
   description = "The id of the bucket."
-  value       = aws_s3_bucket.rafaelsilva4module.id
+  value       = aws_s3_bucket.rafaelsilva4-module.id
 }
 
 
 output "s3_bucket_bucket" {
   description = "The name of the bucket."
-  value       = aws_s3_bucket.rafaelsilva4module.bucket
+  value       = aws_s3_bucket.rafaelsilva4-module.bucket
 }
