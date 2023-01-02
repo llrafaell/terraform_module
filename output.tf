6445ea74ea2063addf7950d6b5351119acb60bc5
Rafael.Goncalves <ragsilva.ciet@rd.com.br>
2023-01-02 10:25:30 -0300

    fix bucked name

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "bucket_arn"{
-    value = aws_s3_bucket.rafaelsilva4_module.arn
+    value = aws_s3_bucket.rafaelsilva4-module.arn
 }
 
 output "s3_bucket_id" {
   description = "The id of the bucket."
-  value       = aws_s3_bucket.rafaelsilva4_module.id
+  value       = aws_s3_bucket.rafaelsilva4-module.id
 }
 
 
 output "s3_bucket_bucket" {
   description = "The name of the bucket."
-  value       = aws_s3_bucket.rafaelsilva4_module.bucket
+  value       = aws_s3_bucket.rafaelsilva4-module.bucket
 }
